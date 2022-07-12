2f942febb161d9956d0d4618d535886faf974727
yuuki <V-YOP@protonmail.com>
2022-07-12 11:22:18 +0800

    EXACT first commit!

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,11 @@
-import React from 'react';
+import React, { Fragment } from 'react';
+import { Button } from 'semantic-ui-react';
 import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => (
+  <Fragment>
+    <Button primary onClick={evt => alert('Hello, World!')}>Click Me</Button>
+  </Fragment>
+)
 
 export default App;
